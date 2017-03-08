1d4feed6ddbc6b6c5b950f405154c13da02de4e5
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-03-08 18:30:09 -0500

    was able to set value for checked boxes (languages)

--- src/main.js
+++ src/main.js
@@ -7,11 +7,37 @@
       .addEventListener('blur', function checkLength(eventObj) {
         console.log(eventObj.target.value);
 
-        
+       if (eventObj.target.value.length) {
+
+       } else {
+         eventObj.target.parentNode.parentNode.classList.add('has-error');
+       }
 
       });
 
+    document.querySelector('#exp')
+        .addEventListener('change', function updateText(eventObj) {
+          console.log(eventObj.target.value);
+          let newText = eventObj.target.value +' Years';
+          eventObj.target.parentNode.childNodes[3].innerText = newText;
+          // eventObj.target.parentNode.childNodes[].innerText = newText;
+
+        });
+  Array.from(document.querySelectorAll('[name="languages"]'))
+        .forEach(function updateNumber(eventObj) {
+          eventObj.addEventListener('change', function updateNumber(eventObj) {
+            console.log(eventObj.target.checked);
+            let checkedCount = 0;
 
 
+            if (eventObj.target.checked) {
+              checkedCount++
+            } else {
+              checkedCount--
+            }
+            eventObj.target.parentNode.parentNode.childNodes[7].innerText = checkedCount + 'languages';
+          });
+
+        });
 
 })();
